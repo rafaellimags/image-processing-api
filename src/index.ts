9a16ed55ff541d60f95181cdc2d8e728e51ae63e
Rafael Firmino <rafael.firmino.lima@acenture.com>
2026-09-23 16:55:15 -0300

    create server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application } from 'express';
+import routes from './routes/index.js';
+
+const app: Application = express();
+const port = 3000;
+
+app.use('/api', routes);
+
+app.listen(port, (): void => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+export default app;
