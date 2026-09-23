c56728d1b6fe5aa7adba45d62d00d27dd0e13b61
Rafael Firmino <rafael.firmino.lima@acenture.com>
2026-09-23 16:56:16 -0300

    create main router

---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response } from 'express';
+import images from './api/images.js';
+
+const routes = Router();
+
+routes.get('/', (req: Request, res: Response): void => {
+  res.send('API de Processamento de Imagens funcionando!');
+});
+
+routes.use('/images', images);
+
+export default routes;
